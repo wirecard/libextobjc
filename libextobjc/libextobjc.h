059dfefe09d5ed615945b2558919733bb70ebce4
Vrana, Jozef <jozef.vrana@ymail.com>
2019-01-28 08:54:20 +0100

    carthage support

---
+++ libextobjc/libextobjc.h
@@ -0,0 +1,20 @@
+//
+//  libextobjc.h
+//  libextobjc
+//
+//  Created by Vrana, Jozef on 28/01/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libextobjc.
+FOUNDATION_EXPORT double libextobjcVersionNumber;
+
+//! Project version string for libextobjc.
+FOUNDATION_EXPORT const unsigned char libextobjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libextobjc/PublicHeader.h>
+#import <libextobjc/EXTScope.h>
+#import <libextobjc/EXTRuntimeExtensions.h>
+#import <libextobjc/metamacros.h>
+
